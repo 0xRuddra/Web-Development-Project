a8cc938e35d70162b198b0c94f12ca673b8714ec
Ruddra <notify.ruddra.cse@gmail.com>
2022-11-24 13:30:47 +0600

    html-css folder uploading

---
+++ HTML-CSS/simple_Windows11_tmp/script.js
@@ -0,0 +1,9 @@
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let img = document.querySelector(".taskbar img");
+console.log(taskbar);
+console.log(img);
+
+taskbar.addEventListener('click',()=>{
+    console.log("click");
+    
+});
